6fef8096507ed8cfabfa1332fab0e882f4093a0c
Dionei Bianchati <dion.biancha@gmail.com>
2023-04-19 18:32:41 -0300

    connect mysql

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,24 +1,33 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-import { Product } from '../models/Product';
+import { sequelize } from "../instances/mysql";
 
-export const home = (req: Request, res: Response)=>{
-    let age: number = 90;
-    let showOld: boolean = false;
+import { Product } from "../models/Product";
 
-    if(age > 50) {
-        showOld = true;
-    }
+export const home = async (req: Request, res: Response) => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexão estabelicida");
+  } catch (error) {
+    console.log("Deu problema", error);
+  }
 
-    let list = Product.getAll();
-    let expensiveList = Product.getFromPriceAfter(12);
+  let age: number = 90;
+  let showOld: boolean = false;
 
-    res.render('pages/home', {
-        name: 'Bonieky',
-        lastName: 'Lacerda',
-        showOld,
-        products: list,
-        expensives: expensiveList,
-        frasesDoDia: []
-    });
+  if (age > 50) {
+    showOld = true;
+  }
+
+  let list = Product.getAll();
+  let expensiveList = Product.getFromPriceAfter(12);
+
+  res.render("pages/home", {
+    name: "Bonieky",
+    lastName: "Lacerda",
+    showOld,
+    products: list,
+    expensives: expensiveList,
+    frasesDoDia: [],
+  });
 };
